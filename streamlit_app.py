7c99f0d709da4d1ae30252c2f54e31678fe74466
Harsha-hub1208 <143723457+Harsha-hub1208@users.noreply.github.com>
2023-08-31 19:25:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
+
 st.write('''Get the highest of the three numbers''')
+st.header('User Input Parameters')
 n1=int(input('Enter the first number: '))
 n2=int(input('Enter the second number: '))
 n3=int(input('Enter the third number: '))
