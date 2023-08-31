c8e6ba9762f9b785011279ecbeaff57326d4213e
Harsha-hub1208 <143723457+Harsha-hub1208@users.noreply.github.com>
2023-08-31 19:50:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ st.header('Get the highest of the three numbers')
 n1=st.number_input('Enter the first number: ')
 n2=st.number_input('Enter the second number: ')
 n3=st.number_input('Enter the third number: ')
-st.return(max(n1,n2,n3))
 
+st.subheader('Result)
 if (n1>n2):
   if (n1>n3):
-    print(n1)
+    st.write(n1)
   else:
-    print(n3)
+    st.write(n3)
 elif(n2> n3):
-  print(n2)
+  st.write(n2)
 else:
-  print(n3)
+  st.write(n3)
